d6349a5cd2a1125c57c8df6c02f20fa52587d7a7
Vitor Chacon <vitorlchacon@gmail.com>
2022-03-13 22:28:23 +0000

    Exercise 2 - Added questions 2 and 3

--- exercise2/src/main/java/com/visualnuts/Exercise2.java
+++ exercise2/src/main/java/com/visualnuts/Exercise2.java
@@ -1,5 +1,9 @@
 package com.visualnuts;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.visualnuts.model.Country;
@@ -10,8 +14,12 @@ public class Exercise2 {
     public static void main(String[] args) {
         final String input = validateInput(args);
         final Country[] countries = validateJSONAndParseCountries(input);
+
+        // Question 1
         totalCountries(countries);
 
+        // Questions 2, 3, 4 and 5
+        evaluateData(countries);
     }
 
     private static String validateInput(String[] args) {
@@ -44,7 +52,50 @@ public class Exercise2 {
     }
 
     public static int totalCountries(Country[] countries) {
-        System.out.println(new StringBuilder().append("There are ").append(countries.length).append(" countries in the world").toString());
+        System.out.println("1 - There are " + countries.length + " countries in the world.");
         return countries.length;
+    }
+
+    private static void evaluateData(Country[] countries) {
+        int countryWithMostLanguagesHavingDEIndex = -1;
+        Set<String> spokenLanguages = new HashSet<>();
+
+        for (int i = 0; i < countries.length; i++) {
+            // Eval for question 2
+            int languageLength = getLanguageLengthHavingDE(countries[i]);
+            if (languageLength > 0) {
+                try {
+                    if (languageLength > countries[countryWithMostLanguagesHavingDEIndex].getLanguages().length) {
+                        countryWithMostLanguagesHavingDEIndex = i;
+                    }
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    // That's the first country which matches DE language
+                    countryWithMostLanguagesHavingDEIndex = i;
+                }
+            }
+
+            // Eval for question 3
+            spokenLanguages.addAll(Arrays.asList(countries[i].getLanguages()));
+        }
+
+        // Question 2
+        if (countryWithMostLanguagesHavingDEIndex > -1) {
+            System.out.println("2 - The country with the most official languages, where they officially speak German is "
+                    + countries[countryWithMostLanguagesHavingDEIndex].getCountry() + ".");
+        } else {
+            System.out.println("2 - There are no countries where they officially speak German.");
+        }
+
+        // Question 3
+        System.out.println("3 - There are " + spokenLanguages.size() + " official languages spoken in the listed countries.");
+    }
+
+    public static int getLanguageLengthHavingDE(Country country) {
+        for (int j = 0; j < country.getLanguages().length; j++) {
+            if (country.getLanguages()[j].equalsIgnoreCase("de")) {
+                return country.getLanguages().length;
+            }
+        }
+        return 0;
     }
 }
