04b041f914de9febc73be8b657a58994df6a9a6c
Vitor Chacon <vitorlchacon@gmail.com>
2022-03-13 20:25:15 +0000

    Exercise 1

--- exercise1/src/test/java/com/visualnuts/Exercise1Test.java
+++ exercise1/src/test/java/com/visualnuts/Exercise1Test.java
@@ -1,5 +1,6 @@
 package com.visualnuts;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -18,5 +19,21 @@ class Exercise1Test {
     @Test
     void exceptionWithStringArgument() {
         assertThrows(IllegalArgumentException.class, () -> Exercise1.main(new String[]{"abc"}));
+    }
+
+    @Test
+    void exceptionWithNegativeNumberArgument() {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> Exercise1.main(new String[] { "0" }));
+        assertEquals(e.getMessage(), "0 not allowed.");
+    }
+
+    @Test
+    void testPositive() {
+        Exercise1.main(new String[] { "500" });
+    }
+
+    @Test
+    void testNegative() {
+        Exercise1.main(new String[] { "-500" });
     }
 }
